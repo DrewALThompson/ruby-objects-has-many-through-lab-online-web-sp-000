f9301874c33fdd9cee4ba850e6813f7a2b7ba11e
Drew Thompson <drewallynthompson@gmail.com>
2020-09-08 23:28:42 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -9,15 +9,11 @@ lass Genre
   end
 
   def songs
-    Song.all.select do |song|
-      song.genre == self
-    end
+    Song.all.select {|song| song.genre == self }
   end
 
   def artists
-    songs.map do |song|
-      song.artist
-    end
+    songs.map {|song| song.artist }
   end
 
   def self.all
