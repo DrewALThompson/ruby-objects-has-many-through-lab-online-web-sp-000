575624b89440f6b72fad520f45c46d8dcbe509ae
Drew Thompson <drewallynthompson@gmail.com>
2020-09-08 23:47:54 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,7 +14,7 @@ class Doctor
   end
   
   def appointments
-    Appointments.all.select {|appointment| appointment.doctors == self}
+    Appointment.all.select {|appointment| appointment.doctors == self}
   end
   
   def new_appointment(date, patient)
